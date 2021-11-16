3554cc84960e26375160d3617bd85fa0c5b2fcc7
dmrickey <dmrickey@gmail.com>
2021-11-15 22:38:34 -0600

    Blue arrow iterative attack

---
+++ spell-animations/blue-arrows.js
@@ -0,0 +1,47 @@
+// requires JB2A patreon and Sequencer
+//   expects the attack is set up using Formulaic Attack
+
+// fail early if no caster
+if (typeof token === 'undefined') return;
+
+let missiles = 3;
+if (typeof item !== 'undefined') {
+    // todo figure out where the info that "I hit single attack" is;
+    missiles = item.data.data?.formulaicAttacks?.count?.value || missiles;
+}
+
+const animation = (distance) => {
+    let d;
+    if (distance <= 5) {
+        d = '05';
+    }
+    else if (distance <= 15) {
+        d = '15';
+    }
+    else if (distance <= 30) {
+        d = '30';
+    }
+    else if (distance <= 60) {
+        d = '60';
+    }
+    else if (distance <= 90) {
+        d = '90';
+    }
+
+    return `jb2a.arrow.physical.blue.${d}ft`;
+};
+
+const targetTokens = [...game.user.targets];
+
+for (let i = 0; i < missiles; i++) {
+    const target = targetTokens[i] || targetTokens[Math.floor(Math.random() * targetTokens.length)];
+    new Sequence()
+        .effect()
+        .delay(i * 250, i * 250 + 175)
+        .JB2A()
+        .file(animation(canvas.grid.measureDistance(token, target)))
+        .atLocation(token)
+        .reachTowards(target)
+        .randomOffset(0.75)
+        .play();
+}
