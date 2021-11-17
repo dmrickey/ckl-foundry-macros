797d9bcc5adedaa6a3be0208b1c52aeaa7faba87
dmrickey <dmrickey@gmail.com>
2021-11-17 10:16:25 -0600

    Edited chakram to use different range animations

--- spell-animations/chakram.js
+++ spell-animations/chakram.js
@@ -1,9 +1,48 @@
 // requires JB2A patreon and Sequencer
 
-// swap this out with your web path
-const autoReturnPath = 'modules/jb2a_patreon/Library/Generic/Weapon_Attacks/Ranged/Chakram01_02_Regular_White_30ft_1600x400.webm';
-const startPath = 'modules/jb2a_patreon/Library/Generic/Weapon_Attacks/Ranged/Chakram01_01_Regular_White_30ft_1600x400.webm';
-const endPath = 'modules/jb2a_patreon/Library/Generic/Weapon_Attacks/Ranged/Chakram01_01_Regular_White_Return_30ft_1600x400.webm';
+// todo just use db path and no distance 
+const returnPaths = {
+    '15': 'modules/jb2a_patreon/Library/Generic/Weapon_Attacks/Ranged/Chakram01_01_Regular_White_Return_15ft_1000x400.webm',
+    '30': 'modules/jb2a_patreon/Library/Generic/Weapon_Attacks/Ranged/Chakram01_01_Regular_White_Return_30ft_1600x400.webm',
+    '60': 'modules/jb2a_patreon/Library/Generic/Weapon_Attacks/Ranged/Chakram01_01_Regular_White_Return_60ft_2800x400.webm',
+    '90': 'modules/jb2a_patreon/Library/Generic/Weapon_Attacks/Ranged/Chakram01_01_Regular_White_Return_90ft_4000x400.webm',
+}
+const throwPaths  = {
+    '15': 'modules/jb2a_patreon/Library/Generic/Weapon_Attacks/Ranged/Chakram01_01_Regular_White_15ft_1000x400.webm',
+    '30': 'modules/jb2a_patreon/Library/Generic/Weapon_Attacks/Ranged/Chakram01_01_Regular_White_30ft_1600x400.webm',
+    '60': 'modules/jb2a_patreon/Library/Generic/Weapon_Attacks/Ranged/Chakram01_01_Regular_White_60ft_2800x400.webm',
+    '90': 'modules/jb2a_patreon/Library/Generic/Weapon_Attacks/Ranged/Chakram01_01_Regular_White_90ft_4000x400.webm',
+}
+const allInOnePaths  = {
+    '15': 'modules/jb2a_patreon/Library/Generic/Weapon_Attacks/Ranged/Chakram01_02_Regular_White_15ft_1000x400.webm',
+    '30': 'modules/jb2a_patreon/Library/Generic/Weapon_Attacks/Ranged/Chakram01_02_Regular_White_30ft_1600x400.webm',
+    '60': 'modules/jb2a_patreon/Library/Generic/Weapon_Attacks/Ranged/Chakram01_02_Regular_White_60ft_2800x400.webm',
+    '90': 'modules/jb2a_patreon/Library/Generic/Weapon_Attacks/Ranged/Chakram01_02_Regular_White_90ft_4000x400.webm',
+}
+
+const distanceKey = (from, to) => {
+    const distance = canvas.grid.measureDistance(from, to);
+    let d;
+    if (distance <= 15) {
+        d = '15';
+    }
+    else if (distance <= 30) {
+        d = '30';
+    }
+    else if (distance <= 60) {
+        d = '60';
+    }
+    else if (distance <= 90) {
+        d = '90';
+    }
+
+    return d || '15';
+};
+
+const getFilePath = (path, from, to) => {
+    const d = distanceKey(from, to);
+    return path[d];
+}
 
 // token filled in below will work if you simply have a token selected and execute the macro manually
 // (it may also work by default depending on your system)
@@ -17,7 +56,7 @@ if (targets.length === 1) {
     new Sequence()
         .effect()
             .JB2A()
-            .file(autoReturnPath)
+            .file(getFilePath(allInOnePaths, token, targets[0]))
             .atLocation(token)
             .reachTowards(targets[0])
             .randomOffset(0.75)
@@ -27,23 +66,31 @@ else {
     const seq = new Sequence()
         .effect()
             .JB2A()
-            .file(startPath)
+            .file(getFilePath(throwPaths, token, targets[0]))
             .atLocation(token)
             .reachTowards(targets[0])
-            .waitUntilFinished(-750)
+            .endTime(450)
+            .waitUntilFinished(-100)
+
     for (let i = 1; i < targets.length; i++) {
+        const from = targets[i-1];
+        const to = targets[i];
         seq.effect()
             .JB2A()
-            .file(startPath)
-            .atLocation(targets[i-1])
-            .reachTowards(targets[i])
-            .waitUntilFinished(-750)
+            .file(getFilePath(throwPaths, from, to))
+            .startTime(550)
+            .atLocation(from)
+            .reachTowards(to)
+            .endTime(1000)
+            .waitUntilFinished(-100)
     }
+
+    const last = targets[targets.length - 1];
     seq.effect()
         .JB2A()
-        .file(endPath)
+        .file(getFilePath(returnPaths, token, last))
+        .startTime(500)
         .atLocation(token)
-        .reachTowards(targets[targets.length - 1])
-        .waitUntilFinished(-750)
+        .reachTowards(last)
     .play();
 }
