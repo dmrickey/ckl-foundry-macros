761b3649bef82e05c43264d67f5b476cad3b33e6
dmrickey <dmrickey@gmail.com>
2021-11-21 02:08:57 -0600

    Moved weapons into new folder

--- weapon-animations/blue-arrows.js
+++ weapon-animations/blue-arrows.js
@@ -12,27 +12,6 @@ if (typeof data !== 'undefined') {
     missiles = data.fullAttack ? missiles : 1;
 }
 
-const animation = (distance) => {
-    let d;
-    if (distance <= 5) {
-        d = '05';
-    }
-    else if (distance <= 15) {
-        d = '15';
-    }
-    else if (distance <= 30) {
-        d = '30';
-    }
-    else if (distance <= 60) {
-        d = '60';
-    }
-    else if (distance <= 90) {
-        d = '90';
-    }
-
-    return `jb2a.arrow.physical.blue.${d}ft`;
-};
-
 const targetTokens = [...game.user.targets];
 
 for (let i = 0; i < missiles; i++) {
@@ -41,7 +20,7 @@ for (let i = 0; i < missiles; i++) {
         .effect()
         .delay(i * 250, i * 250 + 175)
         .JB2A()
-        .file(animation(canvas.grid.measureDistance(token, target)))
+        .file('jb2a.arrow.physical.blue')
         .atLocation(token)
         .reachTowards(target)
         .randomOffset(0.75)
