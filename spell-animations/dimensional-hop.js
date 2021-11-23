de099e2a813ba6222b9819853d6dda69558ec466
dmrickey <dmrickey@gmail.com>
2021-11-23 15:08:47 -0600

    Update dimensional-hop.js

--- spell-animations/dimensional-hop.js
+++ spell-animations/dimensional-hop.js
@@ -1,71 +1,77 @@
 // requires Warpgate, Sequencer, and JB2A patreon module
 
-const distanceTo = (target) => canvas.grid.measureDistances([{ ray: new Ray(token, target) }], { gridSpaces: true });
-
 const distanceAvailable = item.data.data.uses.value * 5;
 
-let travelDistance = 0;
+let crosshairsDistance = 0;
 const checkDistance = async (crosshairs) => {
     while (crosshairs.inFlight) {
         //wait for initial render
         await warpgate.wait(100);
 
         const ray = new Ray(token.center, crosshairs);
-        const targetDistance = canvas.grid.measureDistances([{ ray }], { gridSpaces: true })[0];
+        const distance = canvas.grid.measureDistances([{ ray }], { gridSpaces: true })[0];
 
         //only update if the distance has changed
-        if (travelDistance !== targetDistance) {
-            travelDistance = targetDistance;
-            if (targetDistance > distanceAvailable) {
+        if (crosshairsDistance !== distance) {
+            crosshairsDistance = distance;
+            if (distance > distanceAvailable) {
                 crosshairs.icon = 'icons/svg/hazard.svg';
             } else {
-                crosshairs.icon = token.data.img;
+                crosshairs.icon = item.data.img;
             }
 
             crosshairs.draw();
-            crosshairs.label = `${targetDistance} ft`;
+            crosshairs.label = `${distance} ft`;
         }
     }
 }
 
-const callbacks = {
-    show: checkDistance
-}
-
 const location = await warpgate.crosshairs.show(
     {
+        // swap between targeting the grid square vs intersection based on token's size
         interval: token.data.width % 2 === 0 ? 1 : -1,
         size: token.data.width,
-        icon: token.data.img, label: '0 ft.'
+        icon: item.data.img,
+        label: '0 ft.',
     },
-    callbacks
+    {
+        show: checkDistance
+    },
 );
 
-if (location.cancelled || travelDistance > distanceAvailable) {
+if (location.cancelled || crosshairsDistance > distanceAvailable) {
     return;
 }
 
 const seq = new Sequence().effect()
-    .JB2A()
-    .scale(.5)
+    .scale(.25)
     .endTime(400)
     .file('jb2a.magic_signs.circle.02.conjuration.intro.blue')
-    .waitUntilFinished()
+    .waitUntilFinished(-500)
     .atLocation(token)
     .thenDo(async () => {
-        const { x, y } = canvas.grid.getSnappedPosition(location.x - 10, location.y - 10);
-        await token.document.update({ x, y }, { animate: false });
-
-        const updatedCharges = item.data.data.uses.value - travelDistance / 5;
+        const updatedCharges = item.data.data.uses.value - crosshairsDistance / 5;
         await item.update({ 'data.uses.value': updatedCharges });
     });
+seq.animation()
+    .on(token)
+    .fadeOut(500)
+    .duration(500)
+    .waitUntilFinished();
+seq.animation()
+    .on(token)
+    .teleportTo(location, { relativeToCenter: true })
+    .snapToGrid()
+    .waitUntilFinished();
+seq.animation()
+    .on(token)
+    .fadeIn(1000)
+    .waitUntilFinished(-999);
 seq.effect()
-    .JB2A()
     .file('jb2a.impact.003.blue')
     .atLocation(token)
 seq.effect()
-    .JB2A()
-    .scale(.5)
+    .scale(.25)
     .startTime(400)
     .file('jb2a.magic_signs.circle.02.conjuration.outro.blue')
     .atLocation(token)
