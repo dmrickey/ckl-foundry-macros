6f21da590e4150f0b8455f2c894f1398cdf7fa43
dmrickey <dmrickey@gmail.com>
2021-12-14 15:47:15 -0600

    Update burning-hands.js

--- spell-animations/burning-hands.js
+++ spell-animations/burning-hands.js
@@ -1,31 +1,47 @@
-// const sourceConfig = {
-//   drawIcon: true,
-//   drawOutline: false,
-//   interval: -1,
-//   label: 'Cone Start',
-// }
+// requires warpgate, jb2a patreon, and sequencer
 
-// const source = await warpgate.crosshairs.show(sourceConfig);
-// if (source.cancelled) {
-//   return;
-// }
+// cast from source token, if no source token, then select a square to originate the cone from.
+let source;
+if (typeof token === 'undefined') {
+    const sourceConfig = {
+        drawIcon: true,
+        drawOutline: false,
+        interval: -1,
+        label: 'Cone Start',
+    };
 
-// this will originate from the selected token, the above commented out code will originate it from a selected crosshairs position
-if (!token) {
-    return;
+    source = await warpgate.crosshairs.show(sourceConfig);
+    if (source.cancelled) {
+        return;
+    }
 }
-const source = token.center;
+else {
+    source = token.center;
+}
 
 const gridSize = canvas.grid.h;
 
 const square = {
     x: source.x,
     y: source.y,
-    get center() { return { x: this.x, y: this.y }; },
-    get top() { return this.center.y - gridSize / 2; },
-    get bottom() { return this.center.y + gridSize / 2; },
-    get left() { return this.center.x - gridSize / 2; },
-    get right() { return this.center.x + gridSize / 2; },
+    get center() {
+        return {
+            x: this.x,
+            y: this.y
+        };
+    },
+    get top() {
+        return this.center.y - gridSize / 2;
+    },
+    get bottom() {
+        return this.center.y + gridSize / 2;
+    },
+    get left() {
+        return this.center.x - gridSize / 2;
+    },
+    get right() {
+        return this.center.x + gridSize / 2;
+    },
 };
 
 const templateData = {
@@ -35,7 +51,7 @@ const templateData = {
     fillColor: '#000000',
     angle: 90,
     x: square.center.x,
-    y: source.right,
+    y: source.right || (square.center.x + gridSize / 2),
 }
 
 let template = (await canvas.scene.createEmbeddedDocuments('MeasuredTemplate', [templateData]))[0];
@@ -45,13 +61,14 @@ const targetConfig = {
     drawOutline: false,
 }
 
+const degtorad = (degrees) =>  degrees * Math.PI / 180;
 let currentCrosshairsAngle = 0;
 const updateTemplateLocation = async (crosshairs) => {
     while (crosshairs.inFlight) {
         await warpgate.wait(100);
         const ray = new Ray(square.center, crosshairs);
         const angle = ray.angle * 180 / Math.PI;
-        let closestAngle = (Math.round(angle/45) * 45) % 360;
+        let closestAngle = (Math.round(angle / 45) * 45) % 360;
         if (closestAngle < 0) {
             closestAngle += 360;
         }
@@ -59,7 +76,9 @@ const updateTemplateLocation = async (crosshairs) => {
         if (currentCrosshairsAngle !== closestAngle) {
             currentCrosshairsAngle = closestAngle;
 
-            const update = { direction: closestAngle };
+            const update = {
+                direction: closestAngle
+            };
             switch (closestAngle) {
                 case 315:
                 case 0:
@@ -75,7 +94,7 @@ const updateTemplateLocation = async (crosshairs) => {
                 case 225:
                     update.x = square.left;
                     break;
-          }
+            }
             switch (closestAngle) {
                 case 0:
                 case 180:
@@ -91,23 +110,29 @@ const updateTemplateLocation = async (crosshairs) => {
                 case 315:
                     update.y = square.top;
                     break;
-          }
+            }
 
-          template = await template.update(update);
+            template = await template.update(update);
+            const tokensInTemplate = (targetTemplate) => canvas.tokens.placeables.filter(o => canvas.grid.getHighlightLayer('Template.' + targetTemplate.id).geometry.containsPoint(o.center));
+            const tokensToTarget = tokensInTemplate(template);
+            game.user.updateTokenTargets(tokensToTarget.map(x => x.id));
         }
     }
 }
 
-const target = await warpgate.crosshairs.show(targetConfig, { show: updateTemplateLocation });
-if (target.cancelled) {
+const rotateCrosshairs = await warpgate.crosshairs.show(targetConfig, {
+    show: updateTemplateLocation
+});
+if (rotateCrosshairs.cancelled) {
     await template.delete();
+    game.user.updateTokenTargets();
     return;
 }
 
 const seq = new Sequence();
 seq.effect()
     .file('jb2a.magic_signs.rune.evocation.intro.red')
-    .atLocation(token)
+    .atLocation(source)
     .scaleToObject()
     .scale(1.6)
     .opacity(0.5)
@@ -118,10 +143,15 @@ seq.effect()
     .fadeIn(300)
     .rotate(-currentCrosshairsAngle)
     .size(gridSize * 3)
-    .scale({ x: 1, y: 1.5 })
-    .anchor({ x: 0, y: 0.5 });
-
+    .scale({
+        x: 1,
+        y: 1.5
+    })
+    .anchor({
+        x: 0,
+        y: 0.5
+    })
+    .waitUntilFinished();
 
 await seq.play();
-
 await template.delete();
