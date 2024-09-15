725f9a7448b056c50a9503bed80c46b69165d479
dmrickey <dmrickey@gmail.com>
2024-09-15 16:24:08 -0500

    Updated ioun stone a bit

--- items/ioun-stone.js
+++ items/ioun-stone.js
@@ -1,21 +1,27 @@
 // drop this on an "Equip" script call and change the path for the stone of your choice
 const path = "jb2a.ioun_stones.01.blue.strength";
 
-const name = `${token.data.name}-${item.data.name}`;
+const random = Sequencer.Helpers.random_int_between;
+
+const name = `${token.name}-${token.id}-${item.name}-${item.id}`;
+const fromRotation = random(1, 360);
+const toRotation = fromRotation + 360;
+const randomDuration = 6000 + random(1, 3000);
 if (equipped) {
-    new Sequence()
+  new Sequence()
     .effect(path)
-        .attachTo(token)
-        .fadeIn(500)
-        .fadeOut(500)
-        .scale(0.10)
-        .spriteOffset({ y: token.height/2 })
-        .zeroSpriteRotation(true)
-        .loopProperty("spriteContainer", "rotation", { from: 0, to: 360, duration: 6000 })
-        .persist(true, { persistTokenPrototype: true })
-        .name(name)
+    .attachTo(token)
+    .fadeIn(500)
+    .fadeOut(500)
+    .scale(0.10)
+    .spriteOffset({ y: random(token.h / 4, token.h / 2) })
+    .zeroSpriteRotation(true)
+    .loopProperty("spriteContainer", "rotation", { from: fromRotation, to: toRotation, duration: randomDuration })
+    .loopProperty("sprite", "rotation", { from: -10, to: 10, duration: 500, pingPong: true, ease: 'easeInOutCubic' })
+    .persist(true, { persistTokenPrototype: true })
+    .name(name)
     .play();
-}    
+}
 else {
-    Sequencer.EffectManager.endEffects({ name, object: token });
+  Sequencer.EffectManager.endEffects({ name, object: token });
 }
